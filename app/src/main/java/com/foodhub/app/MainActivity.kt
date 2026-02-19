e7152a2423fd2b5a2f0a613c663752011cbb853d
Partha <parthachakraborty383@gmail.com>
2026-02-19 23:28:01 +0530

    chore: update README and MainActivity

--- app/src/main/java/com/foodhub/app/MainActivity.kt
+++ app/src/main/java/com/foodhub/app/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.foodhub.app.ui.navigation.FoodHubNavGraph
 import com.foodhub.app.ui.theme.FoodHubTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FoodHubTheme {
+                // This is the entry point for our entire UI
                 FoodHubNavGraph()
             }
         }
